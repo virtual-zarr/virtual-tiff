c019fecfaa5e2e256fb99031b023673e4f9aa63d
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-04-06 19:43:34 -0400

    Add gdal autotest file download function

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pytest
 import xarray as xr
 
+
 @pytest.fixture
 def geotiff_file(tmp_path: Path) -> str:
     """Create a NetCDF4 file with air temperature data."""
@@ -10,3 +11,24 @@ def geotiff_file(tmp_path: Path) -> str:
     with xr.tutorial.open_dataset("air_temperature") as ds:
         ds.isel(time=0).rio.to_raster(filepath, driver="COG", COMPRESS="DEFLATE")
     return str(filepath)
+
+
+def download_files():
+    import pandas as pd
+    import pooch
+
+    current_file_path = Path(__file__).resolve()
+    repo_root = current_file_path.parent.parent
+
+    df = pd.read_csv(
+        f"{repo_root}/tests/data/gdal_autotest/files.csv", header=None, names=["file"]
+    )
+    outpath = f"{repo_root}/tests/data/gdal_autotest"
+    for row in df.iterrows():
+        file = f"https://raw.githubusercontent.com/OSGeo/gdal/refs/heads/master/autotest/{row[1].file}"
+        outname = file.split("/")[-1]
+        pooch.retrieve(file, known_hash=None, path=outpath, fname=outname)
+
+
+if __name__ == "__main__":
+    download_files()
