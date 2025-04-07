82be31265089b81e027e6b788022296053b95a5d
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-04-07 15:02:26 -0400

    Progress

--- src/virtual_tiff/imagecodecs.py
+++ src/virtual_tiff/imagecodecs.py
@@ -1,18 +1,21 @@
-# Adapted from https://github.com/zarr-developers/numcodecs/blob/main/numcodecs/zarr3.py
+# Copied and slightly adapted from https://github.com/zarr-developers/numcodecs/blob/main/numcodecs/zarr3.py
 from __future__ import annotations
 
 import asyncio
 from dataclasses import dataclass, replace
-from functools import cached_property
-from typing import Self
+from functools import cached_property, partial
+from typing import Self, TypeVar
 from warnings import warn
+from collections.abc import Callable
 
 import numpy as np
 
 import numcodecs
-from zarr.abc.codec import ArrayArrayCodec
+import imagecodecs
+from zarr.abc.codec import ArrayArrayCodec, ArrayBytesCodec, BytesBytesCodec
 from zarr.core.array_spec import ArraySpec
-from zarr.core.buffer import NDBuffer
+from zarr.core.buffer import Buffer, BufferPrototype, NDBuffer
+from zarr.core.buffer.cpu import as_numpy_array_wrapper
 from zarr.core.common import JSON, parse_named_configuration
 
 CODEC_PREFIX = "imagecodecs."
@@ -64,7 +67,7 @@ class _NumcodecsCodec:
 
     @cached_property
     def _codec(self) -> numcodecs.abc.Codec:
-        return numcodecs.get_codec(self.codec_config)
+        return imagecodecs.get_codec(self.codec_config)
 
     @classmethod
     def from_dict(cls, data: dict[str, JSON]) -> Self:
@@ -84,8 +87,34 @@ class _NumcodecsCodec:
         raise NotImplementedError  # pragma: no cover
 
 
+class _NumcodecsBytesBytesCodec(_NumcodecsCodec, BytesBytesCodec):
+    def __init__(self, **codec_config: dict[str, JSON]) -> None:
+        super().__init__(**codec_config)
+
+    async def _decode_single(
+        self, chunk_bytes: Buffer, chunk_spec: ArraySpec
+    ) -> Buffer:
+        return await asyncio.to_thread(
+            as_numpy_array_wrapper,
+            self._codec.decode,
+            chunk_bytes,
+            chunk_spec.prototype,
+        )
+
+    def _encode(self, chunk_bytes: Buffer, prototype: BufferPrototype) -> Buffer:
+        encoded = self._codec.encode(chunk_bytes.as_array_like())
+        if isinstance(encoded, np.ndarray):  # Required for checksum codecs
+            return prototype.buffer.from_bytes(encoded.tobytes())
+        return prototype.buffer.from_bytes(encoded)
+
+    async def _encode_single(
+        self, chunk_bytes: Buffer, chunk_spec: ArraySpec
+    ) -> Buffer:
+        return await asyncio.to_thread(self._encode, chunk_bytes, chunk_spec.prototype)
+
+
 class _NumcodecsArrayArrayCodec(_NumcodecsCodec, ArrayArrayCodec):
-    def __init__(self, **codec_config: JSON) -> None:
+    def __init__(self, **codec_config: dict[str, JSON]) -> None:
         super().__init__(**codec_config)
 
     async def _decode_single(
@@ -105,6 +134,20 @@ class _NumcodecsArrayArrayCodec(_NumcodecsCodec, ArrayArrayCodec):
         return chunk_spec.prototype.nd_buffer.from_ndarray_like(out)
 
 
+class _NumcodecsArrayBytesCodec(_NumcodecsCodec, ArrayBytesCodec):
+    def __init__(self, **codec_config: dict[str, JSON]) -> None:
+        super().__init__(**codec_config)
+
+    async def _decode_single(
+        self, chunk_buffer: Buffer, chunk_spec: ArraySpec
+    ) -> NDBuffer:
+        chunk_bytes = chunk_buffer.to_bytes()
+        out = await asyncio.to_thread(self._codec.decode, chunk_bytes)
+        return chunk_spec.prototype.nd_buffer.from_ndarray_like(
+            out.reshape(chunk_spec.shape)
+        )
+
+
 # array-to-array codecs ("filters")
 class Delta(_NumcodecsArrayArrayCodec):
     codec_name = f"{CODEC_PREFIX}delta"
@@ -118,6 +161,92 @@ class Delta(_NumcodecsArrayArrayCodec):
         return chunk_spec
 
 
+T = TypeVar("T", bound=_NumcodecsCodec)
+
+
+def _add_docstring(cls: type[T], ref_class_name: str) -> type[T]:
+    cls.__doc__ = f"""
+        See :class:`{ref_class_name}` for more details and parameters.
+        """
+    return cls
+
+
+def _add_docstring_wrapper(ref_class_name: str) -> Callable[[type[T]], type[T]]:
+    return partial(_add_docstring, ref_class_name=ref_class_name)
+
+
+def _make_bytes_bytes_codec(
+    codec_name: str, cls_name: str
+) -> type[_NumcodecsBytesBytesCodec]:
+    # rename for class scope
+    _codec_name = CODEC_PREFIX + codec_name
+
+    class _Codec(_NumcodecsBytesBytesCodec):
+        codec_name = _codec_name
+
+        def __init__(self, **codec_config: dict[str, JSON]) -> None:
+            super().__init__(**codec_config)
+
+    _Codec.__name__ = cls_name
+    return _Codec
+
+
+def _make_array_array_codec(
+    codec_name: str, cls_name: str
+) -> type[_NumcodecsArrayArrayCodec]:
+    # rename for class scope
+    _codec_name = CODEC_PREFIX + codec_name
+
+    class _Codec(_NumcodecsArrayArrayCodec):
+        codec_name = _codec_name
+
+        def __init__(self, **codec_config: dict[str, JSON]) -> None:
+            super().__init__(**codec_config)
+
+    _Codec.__name__ = cls_name
+    return _Codec
+
+
+def _make_array_bytes_codec(
+    codec_name: str, cls_name: str
+) -> type[_NumcodecsArrayBytesCodec]:
+    # rename for class scope
+    _codec_name = CODEC_PREFIX + codec_name
+
+    class _Codec(_NumcodecsArrayBytesCodec):
+        codec_name = _codec_name
+
+        def __init__(self, **codec_config: dict[str, JSON]) -> None:
+            super().__init__(**codec_config)
+
+    _Codec.__name__ = cls_name
+    return _Codec
+
+
+def _make_checksum_codec(
+    codec_name: str, cls_name: str
+) -> type[_NumcodecsBytesBytesCodec]:
+    # rename for class scope
+    _codec_name = CODEC_PREFIX + codec_name
+
+    class _ChecksumCodec(_NumcodecsBytesBytesCodec):
+        codec_name = _codec_name
+
+        def __init__(self, **codec_config: dict[str, JSON]) -> None:
+            super().__init__(**codec_config)
+
+        def compute_encoded_size(
+            self, input_byte_length: int, chunk_spec: ArraySpec
+        ) -> int:
+            return input_byte_length + 4  # pragma: no cover
+
+    _ChecksumCodec.__name__ = cls_name
+    return _ChecksumCodec
+
+
+LZW = _add_docstring(_make_bytes_bytes_codec("lzw", "LZW"), "numcodecs.imagecodecs_lzw")
+
 __all__ = [
     "Delta",
+    "LZW",
 ]
--- src/virtual_tiff/reader.py
+++ src/virtual_tiff/reader.py
@@ -4,10 +4,8 @@ from virtual_tiff.constants import SAMPLE_DTYPES
 import math
 from typing import TYPE_CHECKING, Any, Iterable
 
-import numcodecs.registry as registry
 from zarr.core.sync import sync
 
-from virtualizarr.codecs import numcodec_config_to_configurable
 from virtualizarr.manifests import (
     ChunkManifest,
     ManifestArray,
@@ -29,17 +27,17 @@ def _get_compression(ifd, compression):
     if compression in (2, 3, 4):
         raise NotImplementedError("CCITT compression is not yet supported")
     elif compression == 5:
-        raise NotImplementedError("LZW compression is not yet supported")
+        return dict(name="imagecodecs.lzw", configuration={})
     elif compression in (6, 7):  # 6 is old style, 7 in new style
         raise NotImplementedError("JPEG compression is not yet supported")
     elif compression == 8:  # Deflate (zlib), Adobe variant
-        return registry.get_codec(dict(id="zlib", level=6))
+        return dict(name="numcodecs.zlib", configuration={"level": 6})
     elif compression == 32773:
         return NotImplementedError("Packbits compression is not yet supported")
     elif compression == 50000:
         # Based on https://github.com/OSGeo/gdal/blob/ecd914511ba70b4278cc233b97caca1afc9a6e05/frmts/gtiff/gtiff.h#L106-L112
         level = ifd.other_tags.get("65564", 9)
-        return registry.get_codec(dict(id="zstd", level=level))
+        return dict(name="numcodecs.zstd", configuration={"level": level})
     else:
         raise ValueError(f"Compression {compression} not recognized")
 
@@ -103,14 +101,12 @@ def _construct_manifest_array(*, ifd: ImageFileDirectory, path: str) -> Manifest
     )
     codecs = []
     if ifd.predictor == 2:
-        codecs.append(registry.get_codec(dict(id="imagecodecs.delta", dtype=dtype.str)))
+        codec = dict(name="imagecodecs.delta", configuration={"dtype": dtype.str})
+        codecs.append(codec)
     compression = ifd.compression
     if compression > 1:
         codecs.append(_get_compression(ifd, compression))
 
-    codec_configs = [
-        numcodec_config_to_configurable(codec.get_config()) for codec in codecs
-    ]
     dimension_names = ("y", "x")  # Following rioxarray's behavior
 
     metadata = create_v3_array_metadata(
@@ -118,7 +114,7 @@ def _construct_manifest_array(*, ifd: ImageFileDirectory, path: str) -> Manifest
         data_type=dtype,
         chunk_shape=chunks,
         fill_value=None,  # TODO: Fix fill value
-        codecs=codec_configs,
+        codecs=codecs,
         dimension_names=dimension_names,
     )
     return ManifestArray(metadata=metadata, chunkmanifest=chunk_manifest)
