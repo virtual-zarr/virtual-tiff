5a8cff41391cbc3058c845259160ade82396c76e
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-04-19 16:43:21 -0400

    Manage test data with dvc

--- tests/test_virtual_tiff.py
+++ tests/test_virtual_tiff.py
@@ -14,20 +14,20 @@ def resolve_filepath(file, folder):
 def example_tiffs():
     current_file_path = Path(__file__).resolve()
     repo_root = current_file_path.parent.parent
-    data_dir = repo_root / "tests" / "data"
+    data_dir = repo_root / "tests" / "dvc" / "github"
 
     # Find all .tif files in the data directory
     tif_files = list(data_dir.glob("*.tif"))
     return [file.name for file in tif_files]
 
 
-def gdal_samples():
+def gdal_samples(relative_folder):
     current_file_path = Path(__file__).resolve()
     repo_root = current_file_path.parent.parent
-    data_dir = repo_root / "tests" / "data" / "gdal_autotest"
+    data_dir = repo_root / "tests" / "dvc" / "gdal_autotest" / relative_folder
 
     # Find all .tif files in the data directory
-    tif_files = list(data_dir.glob("*.tif"))
+    tif_files = list(data_dir.glob("**/*.tif"))
     return [file.name for file in tif_files]
 
 
@@ -58,7 +58,7 @@ class TestVirtualTIFF:
     def test_manifest_store_real_examples(self, filename):
         import rioxarray
 
-        filepath = resolve_filepath(filename, folder="tests/data")
+        filepath = resolve_filepath(filename, folder="tests/dvc/github")
         ds = dataset_from_local_file(filepath)
         assert isinstance(ds, xr.Dataset)
         da_expected = rioxarray.open_rasterio(filepath)
@@ -66,17 +66,17 @@ class TestVirtualTIFF:
 
     @pytest.mark.parametrize("filename", example_tiffs())
     def test_virtual_dataset_real_examples(self, filename):
-        filepath = resolve_filepath(filename, folder="tests/data")
+        filepath = resolve_filepath(filename, folder="tests/dvc/github")
         ms = manifest_store_from_local_file(filepath)
         ds = ms.to_virtual_dataset()
         assert isinstance(ds, xr.Dataset)
         # TODO: Add more property tests
 
-    @pytest.mark.parametrize("filename", gdal_samples())
-    def test_manifest_store_gdal_examples(self, filename):
+    @pytest.mark.parametrize("filename", gdal_samples("0/0"))
+    def test_manifest_store_gdal_0(self, filename):
         import rioxarray
 
-        filepath = resolve_filepath(filename, folder="tests/data/gdal_autotest")
+        filepath = resolve_filepath(filename, folder="tests/dvc/gdal_autotest/0/0")
         ds = dataset_from_local_file(filepath)
         assert isinstance(ds, xr.Dataset)
         da_expected = rioxarray.open_rasterio(filepath)
