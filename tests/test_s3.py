338eb152b2314ec960991172b15b9d12ef750e8d
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-05-12 17:16:07 -0400

    Configure S3 credentials

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,6 +1,8 @@
 from conftest import rioxarray_comparison
+import os
 
 
-def test_load_https_dataset_against_rioxarray():
+def test_load_s3_dataset_against_rioxarray():
     filepath = "s3://sentinel-cogs/sentinel-s2-l2a-cogs/12/S/UF/2022/6/S2B_12SUF_20220609_0_L2A/B04.tif"
+    os.environ["AWS_NO_SIGN_REQUEST"] = "True"
     rioxarray_comparison(filepath)
